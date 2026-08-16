c2a648dff34054436476d983373dd83690141f42
Talent Mudemba <CodeDev-io@liveforever2022unlimited@gmail.com>
2026-08-16 16:29:41 +0200

    fix auth issue

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -18,13 +18,6 @@ const LIVEKIT_URL = process.env.LIVEKIT_URL;
 export const revalidate = 0;
 
 export async function POST(req: Request) {
-  // make an exception for the vercel preview environment
-  if (process.env.NODE_ENV !== 'development' && process.env.IS_VERCEL_PREVIEW !== 'true') {
-    throw new Error(
-      'THIS API ROUTE IS INSECURE. DO NOT USE THIS ROUTE IN PRODUCTION WITHOUT AN AUTHENTICATION LAYER.'
-    );
-  }
-
   try {
     if (LIVEKIT_URL === undefined) {
       throw new Error('LIVEKIT_URL is not defined');
